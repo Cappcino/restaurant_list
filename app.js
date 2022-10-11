dc84e6ad0863f8c4d5ecdd7818b190dafdda83fe
cappcino <cloudlesssky19@gmail.com>
2022-10-11 21:48:09 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ const restaurantList = require('./restaurant.json')
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 
+// mongoose
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) 
+
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
